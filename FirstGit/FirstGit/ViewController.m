736af904a4b2b2ad454befb32aca0c0642d48214
杨卫兵 <yangweibing@yangweibingdeMacBook-Pro.local>
2015-12-04 14:56:35 +0800

    Masonry库的引用配置及简单示例

--- FirstGit/FirstGit/ViewController.m
+++ FirstGit/FirstGit/ViewController.m
@@ -7,6 +7,15 @@
 //
 
 #import "ViewController.h"
+//引用Masonry库
+//1.将Masonry.xcodeproj添加到项目中
+//2.在项目配置中：Targets-->General-->Linked Frameworks and Libraries中添加Masonry.iOS库
+//3.在项目配置中：Targets-->Building Settings-->Search Paths-->User Header Search Paths中添加Masonry项目的主文件夹
+//4.添加头文件
+#import "Masonry.h"
+
+//5.为避免Block的循环引用，定义以下宏简化对当前控制器的引用
+#define WS(weakSelf)  __weak __typeof(&*self)weakSelf = self;
 
 @interface ViewController ()
 
@@ -16,7 +25,18 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // 防止block中的循环引用
+    WS(ws);
+    // 初始化view并设置背景
+    UIView *view = [UIView new];
+    view.backgroundColor = [UIColor redColor];
+    [self.view addSubview:view];
+    // 使用mas_makeConstraints添加约束
+    [view mas_makeConstraints:^(MASConstraintMaker *make) {
+        // 添加大小约束（make就是要添加约束的控件view）
+        make.size.mas_equalTo(CGSizeMake(100, 100));
+        // 添加居中约束（居中方式与self相同）
+        make.center.equalTo(ws.view); }];
 }
 
 - (void)didReceiveMemoryWarning {
